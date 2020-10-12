ecb088c8bcfe6fd594e81d2ca14d3a4359a0f7ae
Matej Križanec <matej.krizanec97@gmail.com>
2020-10-12 18:34:47 +0200

    Update jquery

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $(function() {
         $('#time').timepicker({
@@ -15,14 +14,32 @@ $(document).ready(function () {
         var reminder = $("#time").val();
 
         if (taskName == '' || taskName == null) {
+            $("#notification").text('Please insert task description.');
+            $("#task").css({"border": '#FF0000 1px solid'});
+            return;
+        } 
 
+        if (reminder != '' && reminder != null && !reminder.match(/^([01][0-9]|2[0-3]):[0-5][0-9]$/)) {
+            $("#notification").text('Enter valid time for timer.');
+            $("#time").css({"border": '#FF0000 1px solid'});
+            return;
         }
 
-        $("#task-list").append('<li class="list-group-item list-group-item-primary">' + taskName + '</li>');
+        $("#notification").text('');
+
+
+        $("#main").append(
+            '<div class="row justify-content-center">' +
+                '<div class="col">'+
+                    '<li class="list-group-item list-group-item-primary">' + taskName + '</li>'+
+                '</div>'+
+                '<div class="col-6">'+
+                    '<button id="finish-task" type="button" class="btn btn-primary btn-lg">Task done</button>'+
+                    '<button id="finish-task" type="button" class="btn btn-primary btn-lg">Remove task</button>'+
+                '</div>'+
+            '</div>'
+        );
+
+        $("#insert-task").blur();
     });
 });  
-
-function addTask() {
-    
-
-}
