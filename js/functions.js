8aad72e0bc4f627f573235b1a41acc69dfb22e80
Matej Križanec <matej.krizanec97@gmail.com>
2020-10-12 19:00:48 +0200

    Finish and remove buttons

--- js/functions.js
+++ js/functions.js
@@ -13,11 +13,21 @@ $(document).ready(function () {
         var taskName = $("#task").val();
         var reminder = $("#time").val();
 
+        $("#task").css({"border": ''});
+        $("#time").css({"border": ''});
+
+        if ((taskName == '' || taskName == null) && (reminder != '' && reminder != null && !reminder.match(/^([01][0-9]|2[0-3]):[0-5][0-9]$/))) {
+            $("#notification").text('Add your task and enter valid time if you want a reminder.');
+            $("#task").css({"border": '#FF0000 1px solid'});
+            $("#time").css({"border": '#FF0000 1px solid'});
+            return;
+        }
+
         if (taskName == '' || taskName == null) {
-            $("#notification").text('Please insert task description.');
+            $("#notification").text('Add your task.');
             $("#task").css({"border": '#FF0000 1px solid'});
             return;
-        } 
+        }
 
         if (reminder != '' && reminder != null && !reminder.match(/^([01][0-9]|2[0-3]):[0-5][0-9]$/)) {
             $("#notification").text('Enter valid time for timer.');
@@ -27,19 +37,26 @@ $(document).ready(function () {
 
         $("#notification").text('');
 
-
         $("#main").append(
-            '<div class="row justify-content-center">' +
-                '<div class="col">'+
+            '<div id="task_"' + taskName + 'class="row justify-content-center">' +
+                '<div class="col-7">' +
                     '<li class="list-group-item list-group-item-primary">' + taskName + '</li>'+
+                '</div>' +
+                '<div class="col-2">' +
+                    '<div class="button-group">' +
+                        '<button id="finish-task" type="button" class="btn btn-success btn-lg">Finish</button>'+
+                        '<button id="remove-task" type="button" class="btn btn-danger btn-lg">Remove</button>'+
+                    '</div>' +
                 '</div>'+
-                '<div class="col-6">'+
-                    '<button id="finish-task" type="button" class="btn btn-primary btn-lg">Task done</button>'+
-                    '<button id="finish-task" type="button" class="btn btn-primary btn-lg">Remove task</button>'+
-                '</div>'+
-            '</div>'
+            '</div>' +
+            '</br>'
         );
 
         $("#insert-task").blur();
     });
+
+
+
+
+
 });  
